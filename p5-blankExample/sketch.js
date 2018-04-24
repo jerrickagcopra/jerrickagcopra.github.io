bffb5f0a503b1a0b56cffc37bff1c0dae1098a35
jerrickagcopra <33040350+jerrickagcopra@users.noreply.github.com>
2018-04-24 15:06:09 -0700

    statement

--- p5-blankExample/sketch.js
+++ p5-blankExample/sketch.js
@@ -1,25 +1,34 @@
+
+
+let bg, clouds;
+var mario;
+let x = -225;
+let speed = 5;
+
+function preload() {
+  bg = loadImage('overworld_bg.png');
+  clouds = loadImage('superMarioClouds_0.png');
+  mario = loadGif('Suwakoflapflap.gif');
+
+}
+
 function setup() {
+  createCanvas(500, 500);
+//  console.log(clouds.width/2);
+//  console.log(bg.width);
+//  console.log(bg.height);
+}
 
-  // Sets the screen to be 720 pixels wide and 400 pixels high
-  createCanvas(720, 400);
-  background(0);
-  noStroke();
+function draw() {
+  background(200);
+  image(bg,0,0,0,0);
+  image(clouds, x, 10, clouds.width/2, clouds.height/2);
+  image(mario,0,0);
+  x = x + speed;
 
-  fill(204);
-  triangle(18, 18, 18, 360, 81, 360);
-
-  fill(102);
-  rect(81, 81, 63, 63);
-
-  fill(204);
-  quad(189, 18, 216, 18, 216, 360, 144, 360);
-
-  fill(255);
-  ellipse(252, 144, 72, 72);
-
-  fill(204);
-  triangle(288, 18, 351, 360, 288, 360);
-
-  fill(255);
-  arc(479, 300, 280, 280, PI, TWO_PI);
+  if (x >= width) {
+    speed = -speed;
+  } else if (x < -225) {
+    speed = -speed;
+  }
 }
