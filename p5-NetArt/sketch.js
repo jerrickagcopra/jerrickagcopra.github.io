645316277f41c5a745edf4e27482b9afe17ca077
jerrickagcopra <33040350+jerrickagcopra@users.noreply.github.com>
2018-05-16 12:09:24 -0700

    save

--- p5-NetArt/sketch.js
+++ p5-NetArt/sketch.js
@@ -1,28 +1,24 @@
 var canvas;
 
 function preload() {
-  reimu = createImg("https://en.touhouwiki.net/images/1/1d/Th155Reimu.png");
+//  reimu = createImg("https://en.touhouwiki.net/images/1/1d/Th155Reimu.png");;
+bg = loadImage('Th135_Hakurei_Shrine.png');
+reimu =loadImage('Th16Reimu.png');
 }
 
 function setup() {
-  bg = loadImage('Th135_Hakurei_Shrine.png');
-  canvas = createCanvas(window.innerWidth, window.innerHeight);
-;
-  reimu.position(0,0);
-  reimu.size(reimu.width/2,reimu.height/2);
+
+  canvas = createCanvas(1280,720);
+
+//  reimu.position(0,0);
+//  reimu.size(reimu.width/2,reimu.height/2);
 }
 
 function draw() {
   background(bg);
+  image(reimu,0,0,reimu.width/2,reimu.height/2);
+//  createElement(reimu,'https://en.touhouwiki.net/wiki/Reimu_Hakurei')
 //  fill(255);
 //  ellipse(width/2,height/2,100,100);
 
 }
-
-window.onresize = function() {
-  var w = window.innerWidth;
-  var h = window.innerHeight;
-  canvas.size(w,h);
-  width = w;
-  height = h;
-};
